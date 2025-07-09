291b6432c7ea233f05e299dd98f9c395758fa1ad
rajwadkar-riddhesh <rajwadkar.riddhesh@fyntune.com>
2025-07-09 18:09:07 +0530

    CSRF token generation

---
+++ src/main/java/com/example/JWT/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.JWT.controller;
+
+import com.example.JWT.entity.Student;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student("Alice", 85),
+            new Student("Bob", 90),
+            new Student("Charlie", 78)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return students;
+    }
+
+    @GetMapping("/token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student) {
+        students.add(student);
+        return student;
+    }
+}
