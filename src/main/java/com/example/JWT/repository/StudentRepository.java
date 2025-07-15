5fe3232c8c73379947fa5d0b076002587669d994
rajwadkar-riddhesh <rajwadkar.riddhesh@fyntune.com>
2025-07-15 11:51:32 +0530

    Added a new function for checking existing username

--- src/main/java/com/example/JWT/repository/StudentRepository.java
+++ src/main/java/com/example/JWT/repository/StudentRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
