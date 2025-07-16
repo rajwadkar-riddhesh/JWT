124ab57fcc4fe3cbcc4098cfa5d84f08d6137645
rajwadkar-riddhesh <rajwadkar.riddhesh@fyntune.com>
2025-07-16 12:23:09 +0530

    Extended authorization to Admin

--- src/main/java/com/example/JWT/config/SecurityConfig.java
+++ src/main/java/com/example/JWT/config/SecurityConfig.java
@@ -1,27 +1,18 @@
 package com.example.JWT.config;
 
-//import com.example.JWT.service.MyUserDetailsService;
-
 import com.example.JWT.util.JwtUtil;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-////import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -32,12 +23,13 @@ public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
 
-    public SecurityConfig(@Lazy UserDetailsService userDetailsService) {
+    @Lazy
+    public SecurityConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
     }
 
     @Bean
-    public JwtAuthFilter jwtAuthFilter(JwtUtil jwtUtil, @Lazy UserDetailsService userDetailsService) {
+    public JwtAuthFilter jwtAuthFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
         return new JwtAuthFilter(jwtUtil, userDetailsService);
     }
 
@@ -51,39 +43,6 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
-
-    /// /Disabled CSRF, authentication for any request, form login, HTTP basic authentication, and session management(Stateless)
-//        security.csrf(customizer ->
-//                customizer.disable());
-//        security.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        security.formLogin(Customizer.withDefaults());
-//        security.httpBasic(Customizer.withDefaults());
-//        security.sessionManagement(session -> session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS));
-//
-//        return security.build();
-//    }
-
-//    Working with in-memory user details service
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//
-//        UserDetails user1 = User
-//                .withDefaultPasswordEncoder()
-//                .username("amit")
-//                .password("a@123")
-//                .roles("ADMIN")
-//                .build();
-//        UserDetails user2 = User
-//                .withDefaultPasswordEncoder()
-//                .username("sumit")
-//                .password("s@123")
-//                .roles("USER")
-//                .build();
-//
-//    return new InMemoryUserDetailsManager(user1, user2);
-//    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         return http
@@ -91,8 +50,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login", "/auth/**", "/v3/api-docs", "/swagger-ui/index.html").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().hasRole("STUDENT"))
-                .sessionManagement(session -> session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS))
+                        .anyRequest().hasAnyRole("STUDENT", "ADMIN"))
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
